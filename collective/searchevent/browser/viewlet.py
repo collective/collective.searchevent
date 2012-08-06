54e1c943fab81d5d958f0e9ed4f16e1d75001834
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-08-06 13:29:34 +0300

    removed uncommented lines

--- collective/searchevent/browser/viewlet.py
+++ collective/searchevent/browser/viewlet.py
@@ -1,10 +1,8 @@
-from Products.CMFCore.utils import getToolByName
 from Products.CMFPlone.PloneBatch import Batch
 from collective.searchevent.interfaces import IItemDateTime
 from collective.searchevent.interfaces import ISearchEventResults
 from five import grok
 from plone.app.viewletmanager.manager import OrderedViewletManager
-from plone.memoize.instance import memoize
 from zope.component import getMultiAdapter
 from zope.interface import Interface
 
@@ -23,23 +21,6 @@ class BaseSearchEventViewlet(grok.Viewlet):
     grok.baseclass()
     grok.context(Interface)
     grok.viewletmanager(SearchEventResultsViewletManager)
-
-    # def date(self, year, month, day):
-    #     date = None
-    #     if year:
-    #         if day:
-    #             date = '{}/{}/{}'.format(
-    #                 year,
-    #                 month,
-    #                 day,
-    #             )
-    #         else:
-    #             date = '{}/{}/01'.format(
-    #                 year,
-    #                 month,
-    #             )
-    #         date = DateTime(date)
-    #     return date
 
     def results(self, paths=None, limit=0, b_start=0, b_size=10, b_orphan=1):
         """Returns limited number of brains.
@@ -61,50 +42,6 @@ class BaseSearchEventViewlet(grok.Viewlet):
         return getMultiAdapter(
             (self.context, self.request), ISearchEventResults)(
                 paths=paths, limit=limit, b_start=b_start, b_size=b_size, b_orphan=b_orphan)
-        # context = aq_inner(self.context)
-        # catalog = getToolByName(context, 'portal_catalog')
-        # form = self.request.form
-        # after_year = form.get('form.widgets.after_date-year', None)
-        # after_month = form.get('form.widgets.after_date-month', None)
-        # after_day = form.get('form.widgets.after_date-day', None)
-        # after_date = self.date(after_year, after_month, after_day)
-        # before_year = form.get('form.widgets.before_date-year', None)
-        # before_month = form.get('form.widgets.before_date-month', None)
-        # before_day = form.get('form.widgets.before_date-day', None)
-        # before_date = self.date(before_year, before_month, before_day)
-        # if before_date:
-        #     before_date += 1
-        # if not (before_date or after_date):
-        #     after_date = DateTime()
-        # query = dict(
-        #     object_provides=IATEvent.__identifier__,
-        #     SearchableText=form.get('form.widgets.words', ''),
-        #     sort_on='start',
-        #     start={
-        #         'query': [before_date, ],
-        #         'range': 'max',
-        #     },
-        #     end={
-        #         'query': [after_date, ],
-        #         'range': 'min',
-        #     }
-        # )
-        # Subject = form.get('form.widgets.tags', None)
-        # if Subject:
-        #     query.update({'Subject': Subject})
-        # paths = form.get('form.widgets.paths', paths)
-        # if paths:
-        #     query.update({'path': paths})
-        # if limit:
-        #     query.update({'sort_limit': limit})
-        # # Add b_start and b_size to the query.
-        # if b_size:
-        #     query['b_start'] = b_start
-        #     query['b_size'] = b_size + b_orphan
-        # brains = catalog(query)
-        # if limit:
-        #     brains = brains[:limit]
-        # return IContentListing(brains)
 
 
 class SearchEventResultsViewlet(BaseSearchEventViewlet):
@@ -124,29 +61,5 @@ class SearchEventResultsViewlet(BaseSearchEventViewlet):
             start=b_start,
             orphan=b_orphan)
 
-    # @memoize
-    # def _ulocalized_time(self):
-    #     """Return ulocalized_time method.
-
-    #     :rtype: method
-    #     """
-    #     translation_service = getToolByName(self.context, 'translation_service')
-    #     return translation_service.ulocalized_time
-
     def datetime(self, item):
         return IItemDateTime(item)()
-        # start = item.start
-        # end = item.end
-        # ulocalized_time = self._ulocalized_time()
-        # start_dt = ulocalized_time(start, long_format=True, context=self.context)
-        # if start.Date() == end.Date():
-        #     if start == end:
-        #         dt = start_dt
-        #     else:
-        #         end_time = ulocalized_time(end, time_only=True)
-        #         dt = u'{} - {}'.format(start_dt, end_time)
-        # else:
-        #     end_dt = ulocalized_time(end, long_format=True, context=self.context)
-        #     dt = u'{} - {}'.format(start_dt, end_dt)
-
-        # return dt
