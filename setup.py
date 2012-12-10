7cf7923b3e57e7f63e5ba9c8889fdf6978185618
Taito Horiuchi <taito.horiuchi@gmail.com>
2012-12-10 14:13:44 +0900

    Release 0.5

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ long_description = (
 
 setup(
     name='collective.searchevent',
-    version='0.4',
+    version='0.5',
     description="Adds portlet to search event content types for Plone.",
     long_description=long_description,
     classifiers=[
